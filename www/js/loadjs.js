a98f4ed49916b54b96eec5704f2f93031744eea9
thijsbrentjens <thijs@brentjensgeoict.nl>
2012-07-13 11:27:17 +0200

    Opschonen overbodige bestanden

--- www/js/loadjs.js
+++
@@ -1,8 +0,0 @@
-var basepath = "http://nieuwsinkaart.nl/pdok/kaart/";
-$.getScript(basepath+'api/OpenLayers.js', function(){
-	$.getScript(basepath+'api/javascripts/proj4js-compressed.js', function() {
-		$.getScript(basepath+'api/lusc-api.js', function(){
-			// var pdokkaart = createPDOKKaart();
-		});
-	});
-});
